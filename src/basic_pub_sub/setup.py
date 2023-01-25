b94cb63a851434c5653b026ad13bf2a14963c74b
Jarrod Sanders <jarrod.sanders@ufl.edu>
2023-01-25 14:23:28 -0500

    initialized

---
+++ src/basic_pub_sub/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'basic_pub_sub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jarrod Sanders',
+    maintainer_email='jarrod.sanders@ufl.edu',
+    description='A package to demonstrate a basic pub/sub for Autonomous Robotics',
+    license='MIT or D&R',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
