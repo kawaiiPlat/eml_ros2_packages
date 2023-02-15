269e2db35161553a39cc25bc6dd2167656f61df3
Adrian Fernandez <adrian23fernandez@gmail.com>
2023-02-15 15:37:01 -0500

    fixed launch file, i guess

--- project1_demo/setup.py
+++ project1_demo/setup.py
@@ -13,9 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-
-        # Include all launch files.
-        (os.path.join('share', package_name), glob('launch/*_launch.py'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
