fdee674aa3e593b7c13339dbb410cadea8689b2b
Adrian Fernandez <adrian23fernandez@gmail.com>
2023-03-10 17:34:08 -0500

    finished up PID controller for now

--- milestone_4_pid/milestone_4_pid/pid_controller.py
+++ milestone_4_pid/milestone_4_pid/pid_controller.py
@@ -2,7 +2,8 @@ import rclpy
 from rclpy.node import Node
 
 import math
-import numpy as numpy
+import numpy as np
+import csv
 
 from sensor_msgs.msg import PointCloud
 from sensor_msgs.msg import LaserScan
@@ -13,13 +14,19 @@ from ackermann_msgs.msg import AckermannDriveStamped
 class BangBangController(Node):
 
     def __init__(self):
-        super().__init__('BangBangControllerNode')
+        super().__init__('PID_Controller_Node')
         self.publisher_ = self.create_publisher(AckermannDriveStamped, 'vehicle_command_ackermann', 10)
         self.subcriber = self.create_subscription(LaserScan, 'scan', self.sub_callback, 10)
 
         # PID method variables
         self.e_k = []
         self.Sigma = 0
+
+        # For saving output to a text uncomments the following
+        # self.time = 0
+        # self.f = open("print.csv", 'a')
+        # self.rows = csv.writer(self.f,delimiter=',')
+
 
     def sub_callback(self, msg: LaserScan):
                
@@ -28,16 +35,20 @@ class BangBangController(Node):
 
         # hardcode  all other values 
         acker_msg.header = msg.header
-        acker_msg.drive.speed = 1.0
+        acker_msg.drive.speed = 1.30
         acker_msg.drive.acceleration = 0.0
         acker_msg.drive.jerk = 0.0
         acker_msg.drive.steering_angle_velocity = 0.0
 
+        # multiplier scale based on quotient of current speed and originally speed (1.25)
+        # set 1 for now
+        scale = 1 #(acker_msg.drive.speed/1.25)
+
         # set the constants
-        d_setpoint = 0.50
+        d_setpoint = 1
         angle = 30
         coeffient = 1
-        L = 0.020
+        L = 0.035*scale
 
         # get the distances based on constants
         d_offset = msg.ranges[(90+angle)*2]
@@ -59,13 +70,12 @@ class BangBangController(Node):
         error /= d_setpoint 
 
         # PID constants
-        # for now it is "???", they need to be figured out through experimenting
-        K_p = -1.50    #-1*0.50  #??? # , lets say 
-        K_i =  1*0.35  #???
-        K_d = -1*0.05  #???
+        K_p = -1.5 * scale 
+        K_i = 1*0.75 * scale  
+        K_d = 1*0.01 * scale  
 
         # window length
-        window = 100
+        window = 20
 
         # add new error to list
         length = len(self.e_k)
@@ -95,76 +105,11 @@ class BangBangController(Node):
         P = K_p * error
 
         """Integration"""
-        # window range for integration
-        # Sum = 0   
-
-        # if (math.isnan(d_offset) or math.isinf(d_offset) or math.isnan(d) or math.isinf(d)):
-            
-        #     error = self.e_k[len(self.e_k)-1]
-
-        #     # Check if Window is Full.
-        #     if (length >= window) :
-
-        #         # Pop Oldest Value from Integration Window.
-        #         oldest = self.e_k.pop(0)
-        #         self.Sigma -= oldest
-        #         self.Sigma += error
-        #         self.e_k.append(error)
-            
-        #     # Check if Window is not Full.
-        #     else :
-                
-        #         if (len(self.e_k)-1 >= 0):
-
-        #             self.e_k.append(error)
-        #             self.Sigma += error
-
-        #         else:
-
-        #             return
-
-        # else:
-
-        #     # Check if Window is Full.
-        #     if (length >= window) :
-
-        #         # Pop Oldest Value from Integration Window.
-        #         oldest = self.e_k.pop(0)
-        #         self.Sigma -= oldest
-        #         self.Sigma += error
-        #         self.e_k.append(error)
-            
-        #     # Check if Window is not Full.
-        #     else :
-            
-        #         self.e_k.append(error)
-        #         self.Sigma += error
- 
-        # if length < window:
-        #     # add new error value to list and 
-        #     # sigma method var
-        #     self.e_k.append(error)
-        #     self.Sigma += error
-        #     # Sum = Self.Sigma
-
-        #     # update length and k
-        #     length += 1
-        #     k  += 1
-        # else:
-        #     # remove first error in list from 
-        #     # sigma method var and list
-        #     # add new error
-        #     self.Sigma += error - self.e_k[0]
-        #     self.e_k.pop(0)
-        #     self.e_k.append(error)
-        #     # Sum = self.Sigma
-
-        # I = K_i * Sum * 0.1
-        I = (K_i * self.Sigma * 0.1) / len(self.e_k)
+        I = (K_i * self.Sigma * 0.1)
 
         """Derivative"""
         # for now, we will only be using a PI controller
-        # this is if we want to test 
+        # this is if we want to test PID or PD
         D = 0 
         # if length > 1:
         #   D = K_d * (self.e_k[k]-self.e_k[k-1])/0.1 
@@ -172,19 +117,23 @@ class BangBangController(Node):
         # steering angle
         u = (P + I + D)
 
-        #u = u/45
-
-
         # store as steering angle
         acker_msg.drive.steering_angle = u #math.radians(u)
 
-        self.get_logger().info(f"D_perp_L: {D_perp_L}")
-        self.get_logger().info(f"Error: {error}")
-        self.get_logger().info(f"P: {P}")
-        self.get_logger().info(f"I: {I}")
-        self.get_logger().info(f"D: {D}")
-        self.get_logger().info(f"u: {u}")
-        self.get_logger().info(f"\n")
+        # uncomment the following to log outputs
+        # self.get_logger().info(f"D_perp_L: {D_perp_L}")
+        # self.get_logger().info(f"Error: {error}")
+        # self.get_logger().info(f"P: {P}")
+        # self.get_logger().info(f"I: {I}")
+        # self.get_logger().info(f"D: {D}")
+        #self.get_logger().info(f"u: {u}")
+        # self.get_logger().info(f"\n")
+
+        # uncomment the following if u are writing to the file
+        # self.time += 0.1
+        # self.rows.writerow([str(self.time),str(d_setpoint),str(error),str(u)])
+        # self.f.flush()
+        
         # publish msg
         self.publisher_.publish(acker_msg)
                 
@@ -197,6 +146,9 @@ def main(args=None):
         rclpy.spin(my_node)
     except KeyboardInterrupt:
         my_node.get_logger().info(" Keyboard Interrupt has occured. Exiting Program now.")
+
+    # uncomment the following if writing to a file
+    # my_node.f.close()
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
